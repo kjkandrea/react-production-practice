d5ec6ffa03cdbd5757a485faf2fb6b8926c6bcc5
kjkandrea <kjkandrea@gmail.com>
2021-08-22 13:32:11 +0900

    Add state display
    쉬운데??

--- src/views/Products.js
+++ src/views/Products.js
@@ -1,15 +1,16 @@
+import { useState } from 'react'
+
 import Header from '../components/global/Header'
 import Footer from '../components/global/Footer'
 
 const Products = () => {
-  const setItems = items => {
-    console.log(items)
-  }
+  const [ items, setItems ] = useState('');
 
   return (
     <>
       <Header setItems={setItems} />
-      <h1>아이템 내놔</h1>
+        <h1>아이템 내놔</h1>
+        {items}
       <Footer/>
     </>
   )
