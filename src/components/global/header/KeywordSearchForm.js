1ac94ec42bd80effdedab6cd8a70a5c8298c8de7
kjkandrea <kjkandrea@gmail.com>
2021-08-22 13:05:22 +0900

    Add submit evt bind

--- src/components/global/header/KeywordSearchForm.js
+++ src/components/global/header/KeywordSearchForm.js
@@ -1,7 +1,16 @@
-const KeywordSearchForm = () => (
-  <form>
-    <h2>귀여운 서치폼</h2>
-  </form>
-)
+const KeywordSearchForm = () => {
+  const submit = e => {
+    e.preventDefault()
+    alert('funk you')
+  }
+
+  return (
+    <form onSubmit={submit}>
+      <h2>귀여운 서치폼</h2>
+      <input type="text" />
+      <button type="submit">상품 검색</button>
+    </form>
+  )
+}
 
 export default KeywordSearchForm;
